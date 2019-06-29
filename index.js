bd231ba4d61f3b57ff41e6a95de004b905566e65
MaximLeskovets <leskovstudio@gmail.com>
2019-06-29 17:46:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const graph = [
+    [1, 2],
+    [0, 3],
+    [0, 4],
+    [1, 5],
+    [2, 6],
+    [3, 7],
+    [4, 9],
+    [5, 8],
+    [7, 9],
+    [6, 8]
+];
+const markedVertices = [0, 5, 9];
+
+const findShortestMarkedPathLength = (graph, markedVertices) => {
+    return true;
+};
+
+console.log(findShortestMarkedPathLength(graph, markedVertices));
